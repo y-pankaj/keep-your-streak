17f7c055cadf74cadc0837ebdd84fab3ddc038d4
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-04-29 16:22:55 +0530

    MongoDB connected

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import CalendarDate from '../components/calendar-date'
+import { signIn, signOut } from 'next-auth/client'
 
 export default function Home() {
   return (
     <div>
       <CalendarDate date="3"></CalendarDate>
+      <button onClick={() => signIn('google')}>Sign in</button>
+      <button onClick={() => signOut()}>Sign out</button>
     </div>
   )
 }
