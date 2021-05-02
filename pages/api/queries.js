1a86515a80c51e8ff78450e0c4b0dec0e9ddd5ad
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-05-02 17:28:13 +0530

    GET and POST for queries completed

--- pages/api/queries.js
+++ pages/api/queries.js
@@ -7,19 +7,47 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
+  const { method } = req;
   const session = await getSession({ req });
   const email = session.user.email;
   const query = { email: email };
-  const update = { $push: { dates: "This is a date" } }
-  let result = await req.db.collection('CalendarRecord')
-                          .update( query, update )
-  
-  if(!result.nModified){
-    let toInsert = { email: email, dates: ["random"]}
-    let result = await req.db.collection('CalendarRecord')
-                                  .insert( toInsert )
+
+  switch(method) {
+    case 'GET':
+      try {
+        const calendarDates = await req.db.collection('CalendarRecord')
+                                          .findOne({ email: email})
+        
+        res.status(200).json({ success: true, data: calendarDates})
+      } catch (error) {
+          res.status(400).json({success: false, data: error})
+      }
+      break;
+    case 'POST':
+      try {
+        const update = { $push: { dates: "This is a date" } }
+        let { result } = await req.db.collection('CalendarRecord')
+                                  .update( query, update )
+        
+        if(!result.nModified){
+          let toInsert = { email: email, dates: ["random"]}
+          result = await req.db.collection('CalendarRecord')
+                                        .insert( toInsert )
+        }
+        res.status(201).json({ success: true, data: result })
+      } catch (error) {
+          res.status(400).json({success: false, data: error})
+      }
+      break;
+    default:
+      res.status(400).json({success: false, data: error})
   }
-  res.json({ "message": "ok"});
 });
 
 export default handler;
+
+
+/* 
+TODO
+Error handling
+*/
