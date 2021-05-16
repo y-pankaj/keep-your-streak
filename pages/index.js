5108ef9bd08f7112661672523e953e8277ea0a7f
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-05-16 13:27:28 +0530

    hide todo on clicking cross

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,11 @@ export default function Home() {
       .then((response) => setTodoList(response.data.todoList));
   }, []);
 
+  function hideTodo() {
+    const todoContainer = document.querySelector(".todo-container");
+    todoContainer.classList.add("hidden");
+  }
+
   if (loading) return <div>loading...</div>;
 
   return (
@@ -41,7 +46,24 @@ export default function Home() {
         </>
       )} */}
       </div>
-      <div className="absolute py-16 w-full h-full bg-opacity-50 bg-gray-100">
+      <div className="absolute py-16 w-full h-full bg-opacity-50 bg-gray-100 todo-container">
+        <button className="absolute right-8 top-8" onClick={hideTodo}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 bg-yellow-400 rounded-full"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+
         <DateInfo date={date} todoList={todoList} setTodoList={setTodoList} />
       </div>
     </>
