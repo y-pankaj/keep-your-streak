d7a4fafa054db344b9c5a3b9055aec598f48b916
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-05-22 23:24:08 +0530

    completed landing page design

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,32 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
 export default function Home() {
+  const router = useRouter();
   const [session, loading] = useSession();
 
-  function handleClick() {
-    const result = fetch("/api/todo")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-    // console.log(result.data);
+  if (loading) return <div></div>;
+
+  if (session) {
+    router.push("/app");
   }
   return (
     <div>
       <Navbar />
-
-      {!loading && !session && (
-        <>
-          <button onClick={() => signIn("google")}>Sign in</button>
-        </>
-      )}
-
-      {!loading && session && (
-        <>
-          <img src={session.user.image} className="avatar" />
-          <h1>{session.user.name}</h1>
-          {JSON.stringify(session)}
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )}
-
-      <button onClick={handleClick}>Press</button>
+      <div className="w-screen h-screen flex flex-col items-center justify-center px-4 bg-stone-henge bg-cover">
+        <div className="my-8">
+          <h1 className="text-6xl text-center font-semibold">
+            Productivity to the Moon{" "}
+            <Image src="/rocket.png" width={50} height={50} />
+          </h1>
+        </div>
+        <button className="py-2 text-2xl bg-yellow-300 rounded-lg w-48">
+          Get Started
+        </button>
+      </div>
     </div>
   );
 }
