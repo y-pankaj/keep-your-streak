c5fe1a669ff8e98402cc6dc701302b78bc921786
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-05-22 21:38:41 +0530

    renamed files for better homepage url

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,23 @@
-import { useState, useEffect } from "react";
-import Calendar from "../components/calendar";
-import DateInfo from "../components/date-info";
-import { signIn, signOut, useSession } from "next-auth/client";
+import React from "react";
 import Navbar from "../components/navbar";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
   const [session, loading] = useSession();
 
-  const [date, setDate] = useState("");
-  const [todoList, setTodoList] = useState([]);
-
-  useEffect(() => {
+  function handleClick() {
     const result = fetch("/api/todo")
       .then((response) => response.json())
-      .then((response) => setTodoList(response.data.todoList));
-  }, []);
-
-  function toggleTodo() {
-    const todoContainer = document.querySelector(".todo-container");
-    todoContainer.classList.toggle("hidden");
+      .then((data) => console.log(data));
+    // console.log(result.data);
   }
-
-  function checkTarget(e) {
-    if (e.target.classList.contains("todo-container")) {
-      toggleTodo();
-    }
-  }
-
-  if (loading) return <div>loading...</div>;
-
   return (
-    <>
+    <div>
       <Navbar />
-      <div className="bg-purple-500">
-        <div className="absolute m-auto h-screen w-screen py-20 ">
-          <div className="table mx-auto">
-            <div className="table-row-group">
-              <Calendar setDate={setDate} toggleTodo={toggleTodo} />
-            </div>
-          </div>
 
-          {/* {!loading && !session && (
+      {!loading && !session && (
         <>
-          <button onClick={() => signIn('google')}>Sign in</button>
+          <button onClick={() => signIn("google")}>Sign in</button>
         </>
       )}
 
@@ -53,32 +28,9 @@ export default function Home() {
           {JSON.stringify(session)}
           <button onClick={() => signOut()}>Sign out</button>
         </>
-      )} */}
-        </div>
-        <div
-          className="fixed overflow-auto pt-24 pb-10 w-full h-full bg-opacity-50 bg-gray-100 todo-container"
-          onClick={checkTarget}
-        >
-          <button className="absolute right-8 top-8" onClick={toggleTodo}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 bg-yellow-400 rounded-full"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
+      )}
 
-          <DateInfo date={date} todoList={todoList} setTodoList={setTodoList} />
-        </div>
-      </div>
-    </>
+      <button onClick={handleClick}>Press</button>
+    </div>
   );
 }
