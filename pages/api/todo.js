a87544ea7e4cd2611a3381e553fffd833ef9280a
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-05-25 18:03:53 +0530

    completed PUT backend

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -39,7 +39,7 @@ handler.post(async (req, res) => {
       .collection("CalendarRecord")
       .updateOne(query, update);
 
-    res.status(201).json({ success: true, data: result });
+    res.status(201).json({ success: true, data: "success" });
   } catch (error) {
     res.status(400).json({ success: false, data: error });
   }
@@ -51,14 +51,35 @@ handler.delete(async (req, res) => {
   const body = req.body;
   const createdAt = JSON.parse(body).createdAt;
   try {
-    const todoList = await req.db
+    const result = await req.db
       .collection("CalendarRecord")
       .updateOne(
         { email: email },
         { $pull: { todoList: { createdAt: createdAt } } }
       );
 
-    res.status(200).json({ success: true, data: todoList });
+    res.status(200).json({ success: true, data: "success" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false, data: error });
+  }
+});
+
+handler.put(async (req, res) => {
+  const session = await getSession({ req });
+  const email = session.user.email;
+  const body = req.body;
+  const createdAt = JSON.parse(body).createdAt;
+  const done = JSON.parse(body).done;
+  try {
+    const result = await req.db
+      .collection("CalendarRecord")
+      .updateOne(
+        { email: email, "todoList.createdAt": createdAt },
+        { $set: { "todoList.$.done": done } }
+      );
+
+    res.status(200).json({ success: true, data: "success" });
   } catch (error) {
     res.status(400).json({ success: false, data: error });
   }
