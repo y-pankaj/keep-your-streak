319a62023e79ab334f1b72dd934c32c53a5ae75f
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-05-29 11:14:43 +0530

    removed stale imports

--- pages/app.js
+++ pages/app.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "../components/calendar";
 import DateInfo from "../components/date-info";
-import { signIn, signOut, useSession, getSession } from "next-auth/client";
+import { getSession } from "next-auth/client";
 import Navbar from "../components/navbar";
 
 export default function App() {
