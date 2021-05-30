a800dec5c744d542879aedd319ba14542b67fcae
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-05-30 18:47:04 +0530

    get study session for month period working

--- pages/api/timer.js
+++ pages/api/timer.js
@@ -6,6 +6,38 @@ const handler = nextConnect();
 
 handler.use(middleware);
 
+handler.get(async (req, res) => {
+  try {
+    const session = await getSession({ req });
+    const email = session.user.email;
+    const start = Number(req.query.start);
+    const end = Number(req.query.end);
+    const startDate = new Date(start).toISOString();
+    const endDate = new Date(end).toISOString();
+
+    const studySessions = await req.db.collection("CalendarRecord").findOne(
+      { email: email },
+      {
+        projection: {
+          timer: {
+            $elemMatch: {
+              date: {
+                $gte: startDate,
+                $lt: endDate,
+              },
+            },
+          },
+        },
+        _id: 0,
+      }
+    );
+
+    res.status(200).json({ success: true, data: studySessions });
+  } catch (error) {
+    res.status(400).json({ success: false, data: error });
+  }
+});
+
 handler.post(async (req, res) => {
   try {
     const session = await getSession({ req });
