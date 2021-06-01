a7da4724dac7c1054106db32b317f35c9cd9c20c
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-06-01 18:39:32 +0530

    GET, POST methods completed

---
+++ pages/api/lists.js
@@ -0,0 +1,47 @@
+import { getSession } from "next-auth/client";
+import nextConnect from "next-connect";
+import middleware from "../../middleware/database";
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  try {
+    const session = await getSession({ req });
+    const email = session.user.email;
+    const lists = await req.db
+      .collection("CalendarRecord")
+      .findOne({ email: email }, { projection: { lists: 1, _id: 0 } });
+
+    if (lists === null) {
+      const result = await req.db
+        .collection("CalendarRecord")
+        .insertOne({ email: session.user.email, lists: [], timer: [] });
+
+      res.status(200).json({ success: true, data: result.ops[0] });
+    } else {
+      res.status(200).json({ success: true, data: lists });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, data: JSON.stringify(error) });
+  }
+});
+
+handler.post(async (req, res) => {
+  try {
+    const session = await getSession({ req });
+    const email = session.user.email;
+    const query = { email: email };
+    const body = req.body;
+    const update = { $push: { lists: body } };
+    await req.db.collection("CalendarRecord").updateOne(query, update);
+
+    res.status(201).json({ success: true, data: "success" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false, data: error });
+  }
+});
+
+export default handler;
