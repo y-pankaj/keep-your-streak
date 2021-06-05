95c1612bf455f5732784e84cee1ac3e50fda0052
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2021-06-05 12:41:29 +0530

    changed study time to hh:mm:ss format

--- components/date-stats.js
+++ components/date-stats.js
@@ -16,20 +16,24 @@ export default function DateStats({ date, timerData }) {
 
   function secondsToHms(d) {
     d = Number(d);
-    var h = Math.floor(d / 3600);
-    var m = Math.floor((d % 3600) / 60);
-    var s = Math.floor((d % 3600) % 60);
+    var h = String(Math.floor(d / 3600));
+    var m = String(Math.floor((d % 3600) / 60));
+    var s = String(Math.floor((d % 3600) % 60));
 
-    var hDisplay = h > 0 ? h + (h == 1 ? " hr: " : " hrs ") : "";
-    var mDisplay = m > 0 ? m + (m == 1 ? " min: " : " mins ") : "";
-    var sDisplay = s > 0 ? s + (s == 1 ? " sec" : " secs") : "";
+    // var hDisplay = h > 0 ? h + (h == 1 ? " hr: " : " hrs ") : "";
+    // var mDisplay = m > 0 ? m + (m == 1 ? " min: " : " mins ") : "";
+    // var sDisplay = s > 0 ? s + (s == 1 ? " sec" : " secs") : "";
+
+    var hDisplay = h.length == 1 ? "0" + h + ":" : h + ":";
+    var mDisplay = m.length == 1 ? "0" + m + ":" : m + ":";
+    var sDisplay = s.length == 1 ? "0" + s : s;
     var totalTime = hDisplay + mDisplay + sDisplay;
-    return totalTime == "" ? "0 sec" : totalTime;
+    return totalTime;
   }
   return (
     <div>
-      <div className="flex flex-row justify-around py-2 text-lg">
-        <span>Total time: {currentStats.time}</span>
+      <div className="flex flex-row justify-around py-2 text-xl">
+        <span>Total study time: {currentStats.time}</span>
         {/* <span>No. of sessions: {currentStats.sessions}</span> */}
       </div>
     </div>
